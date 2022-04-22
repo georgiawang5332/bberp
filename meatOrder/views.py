ecbfdcbd1492f1af498640e3c672707c7f667972
etrex kuo <georgiawang5332@gmail.com>
2022-04-22 22:59:14 +0800

    下面查詢訂單的ForeignKey可以搜尋了 => product_name__meatName__icontains

--- meatOrder/views.py
+++ meatOrder/views.py
@@ -5,13 +5,33 @@ from .filters import *
 
 # Create your views here.
 def listOrder(request):
-    # form = StoreSearchForm(request.POST or None)
+    q_list = MeatOrder.objects.all()
 
-    q_list = MeatOrder.objects.all()#.filter(product_name__icontains=form['product_name'].value(), supplier_name__icontains=form['supplier_name'].value())
+    # order_number = request.GET["order_number"]
+    # product_name = request.GET['product_name']
+    # quantity = request.GET['quantity']
+    # supplier_name = request.GET['supplier_name']
+    # procurement_staff = request.GET['procurement_staff']
+    # add_date = request.GET['add_date']
+    # mod_date = request.GET['mod_date']
+    # if order_number is not None:
+    #     data = MeatOrder.objects.filter(
+    #         order_number__icontains=order_number,
+    #         # product_name__meatName__icontains=product_name,
+    #         # quantity__icontains=quantity,
+    #         # supplier_name__supplier_name__icontains=supplier_name,
+    #         # procurement_staff__icontains=procurement_staff,
+    #         # add_date__icontains=add_date,
+    #         # mod_date__icontains=mod_date
+    #     )
+
+    # if order_number in request.POST:
+    #     data = q_list.filter(
+    #         Q(order_number__icontains=order_number)
+    #     )
+
     q = request.GET.get("q")
-    getobject = request.GET['search']
-    getcategory = request.GET['category']
-
+    q_list = MeatOrder.objects.all()  # .filter(product_name__icontains=form['product_name'].value(), supplier_name__icontains=form['supplier_name'].value())
     if q:
         data = q_list.filter(
             Q(order_number__icontains=q) |
@@ -19,9 +39,9 @@ def listOrder(request):
             Q(quantity__icontains=q) |
             Q(supplier_name__supplier_name__icontains=q) |
             Q(procurement_staff__icontains=q)
-        ).distinct()#.order_by('-mod_date')
+        ).distinct()  # .order_by('-mod_date')
     else:
-        data = MeatOrder.objects.all()#.order_by('-mod_date')
+        data = MeatOrder.objects.all()  # .order_by('-mod_date')
     context = {
         'data': data,
     }
